2e4d06a76ba5c162bdda2f57fbdb80bfe8a37a89
Okan Aslan <okannaslann@hotmail.com>
2022-06-24 20:09:59 +0300

    Initial Commit

---
+++ PastToNowF1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PastToNowF1
+//
+//  Created by MacBook on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
